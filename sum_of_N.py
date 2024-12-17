bc71471d4d6a840a0fc597cb5101fa42aa134ea3
byazid <byazid@Byazids-MacBook-Air.local>
2024-12-18 01:30:05 +0600

    keep prac

---
+++ sum_of_N.py
@@ -0,0 +1,18 @@
+times = int(input("Please input how many times you want to calculate :"))
+
+max_sum = 0
+temp = 0
+my_list = []
+for i in range(times):
+    user = input("Please input something by space")
+    checklist = list(map(int, user.split(" ")))
+    check_sum = sum(checklist)
+    
+    if check_sum > temp:
+        temp = check_sum
+        my_list = checklist
+
+print(temp,my_list)
+
+
+    
